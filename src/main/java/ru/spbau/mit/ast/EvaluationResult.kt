091620a4cc0148de13258c97e740ef462fd6a84d
Ilya Kaysin <demarkok@gmail.com>
2017-11-19 02:21:15 +0300

    clean up

--- src/main/java/ru/spbau/mit/ast/EvaluationResult.kt
+++ src/main/java/ru/spbau/mit/ast/EvaluationResult.kt
@@ -1,26 +1,3 @@
-/*
-TODO: remove it
-
-
-package ru.spbau.mit.ast
-
-
-interface EvaluationResult {
-    fun isPresent(): Boolean
-}
-
-abstract class AbstractEvaluationResult(private val isPresent: Boolean): EvaluationResult {
-    override fun isPresent(): Boolean {
-        return isPresent
-    }
-}
-
-class Value(val value: Int): AbstractEvaluationResult(true)
-
-object None: AbstractEvaluationResult(false)
-
-*/
-
 package ru.spbau.mit.ast
 
 
