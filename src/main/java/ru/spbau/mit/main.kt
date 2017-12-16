1c01d60ee8289d5064c29cd20aa4935fccab3eb2
Ilya Kaysin <demarkok@gmail.com>
2017-12-16 22:12:10 +0300

    add parsing exception handling

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,8 +1,14 @@
 package ru.spbau.mit
 
+import org.antlr.v4.runtime.misc.ParseCancellationException
 import ru.spbau.mit.ast.printer.printFileAST
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
-    printFileAST(Paths.get(args.first()), System.out)
+
+    try {
+        printFileAST(Paths.get(args.first()), System.out)
+    } catch (e: ParseCancellationException) {
+        print(e.message)
+    }
 }
